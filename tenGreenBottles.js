f921886a5696025f01bf59e80bc9e3b095703b8a
Esther Cho <helloecho12345@gmail.com>
2022-04-22 14:58:50 +0100

    initial setup and first passing test of returning first line of the verse with hard coding

---
+++ tenGreenBottles.js
@@ -0,0 +1,5 @@
+const tenGreenBottles = () => {
+  return "Two green bottles"
+}
+
+module.exports = tenGreenBottles;
---
+++ tenGreenBottles.test.js
@@ -0,0 +1,7 @@
+const tenGreenBottles = require('./tenGreenBottles')
+
+describe('tenGreenBottles', () => {
+  it('returns the first line of the verse of the song', () => {
+    expect(tenGreenBottles("Two green bottles")).toEqual("Two green bottles")
+  });
+})
