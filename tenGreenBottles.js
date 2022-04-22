210086766126ae8cb78342c8c42cf45cae88f4ae
Esther Cho <92740628+helloecho12345@users.noreply.github.com>
2022-04-22 15:33:53 +0100

    passes first test where last verse is returned if n=1

--- tenGreenBottles.js
+++ tenGreenBottles.js
@@ -1,5 +1,5 @@
 const tenGreenBottles = () => {
-  return "Two green bottles"
+  return "One green bottle\nHanging on the wall\nOne green bottle\nHanging on the wall\nIf that one green bottle\nShould accidentally fall\nThere'll be no green bottles\nHanging on the wall"
 }
 
 module.exports = tenGreenBottles;
--- tenGreenBottles.test.js
+++ tenGreenBottles.test.js
@@ -1,9 +1,9 @@
 const tenGreenBottles = require('./tenGreenBottles')
 
 describe('tenGreenBottles', () => {
-  it('returns the first line of the verse of the song', () => {
-    expect(tenGreenBottles("Two green bottles")).toEqual("Two green bottles")
-  });
+  it("returns the last verse if n=1", () => {
+    expect(tenGreenBottles(1)).toEqual("One green bottle\nHanging on the wall\nOne green bottle\nHanging on the wall\nIf that one green bottle\nShould accidentally fall\nThere'll be no green bottles\nHanging on the wall")
+  })
 
-  
+
 })
